4256163c83e821b89a7561e68602a1428a5e3d06
Schilders,Koen K.N.D <koen.schilders@student.fontys.nl>
2015-11-06 12:36:38 +0100

    Test push

---
+++ Class_Layer/Chatroom.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Database_Layer;
+
+namespace Class_Layer
+{
+    public class Chatroom
+    {
+        public int ChatroomID { get; private set; }
+        public List<Gebruiker> Users;
+
+        private Data data;
+
+
+        public Chatroom()
+        {
+            this.Users = new List<Gebruiker>();
+
+            // Check if chatroom exists
+                // if false
+                    // create new chatroom
+                    // get chatroomID from database
+                // else
+                    // get chatroomID from database
+
+            // Get ChatroomID from database
+            Database.ExecuteQuery("SELECT CHATROOMID FROM USER_CHATROOM WHERE USERID = 10001");
+        }
+    }
+}
